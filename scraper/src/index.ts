5775309b1d1720b4add86cad03f25196ab6e79bd
BeNikk <bhattnik442@gmail.com>
2025-11-05 21:28:01 +0530

    add:basic scraping

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,2 +1,13 @@
-// playwright scraping 
+import playwright from 'playwright';
 
+(async () => {
+  for (const browserType of ['chromium']) {
+    const browser = await playwright["chromium"].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://amazon.com');
+    await page.screenshot({ path: `nodejs_${browserType}.png`, fullPage: true });
+    await page.waitForTimeout(1000);
+    await browser.close();
+  }
+})();
